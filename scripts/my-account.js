ff62c39372d1ca638bc8ee2e911e8ee9c7f5cf4e
ajuteixeira <teixeirajuuh@gmail.com>
2024-07-11 17:31:41 -0300

    ajustes

---
+++ scripts/my-account.js
@@ -0,0 +1,12 @@
+var slider = tns({
+  container: ".fixedWidth",
+  fixedWidth: 164,
+  gutter: 15,
+  swipeAngle: false,
+  speed: 200,
+  loop: false,
+  mouseDrag: true,
+  autoplayButtonOutput: false,
+  controls: false,
+  nav: false,
+});
