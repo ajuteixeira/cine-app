d2198cb2e6ab884ae6c14531d12ec943c59f51c2
ajuteixeira <teixeirajuuh@gmail.com>
2024-07-11 17:17:30 -0300

    ajuste no slider da página my-account

---
+++ scripts/my-account.js
@@ -0,0 +1,12 @@
+var slider = tns({
+  container: ".fixedWidth",
+  fixedWidth: 222,
+  gutter: 15,
+  swipeAngle: false,
+  speed: 200,
+  loop: false,
+  mouseDrag: true,
+  autoplayButtonOutput: false,
+  controls: false,
+  nav: false,
+});
